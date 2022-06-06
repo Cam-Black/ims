ee92dd03ddd12bdb8d41444974c97c53f095b91a
Cam-Black <cameronablack1998@gmail.com>
2022-06-06 17:19:15 +0100

    IMS-24 IMS-26 IMS-30 #in-progress #comment All tables created, now need to create DAO Classes in Java. Also testing joining tables to see relevant info such as item_names next to item_ids

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,4 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `ims`.`items` (`item_name`, `item_cost`) VALUES ("Call of Duty: Modern Warfare", 20.14);
+INSERT INTO `ims`.`orders` ( `fk_customer_id`) VALUES (1);
+INSERT INTO `ims`.`order_items` (`fk_order_id`, `fk_item_id`) VALUES (1, 1);

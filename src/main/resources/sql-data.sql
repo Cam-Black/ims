e413c26d38dcf88bdad2c88af1da85cfb428fcd8
Cam-Black <cameronablack1998@gmail.com>
2022-06-06 16:53:29 +0100

    IMS-24 IMS-26 IMS-30 #in-progress #comment Created base tables for orders and items, will need fine tuning and started creating order_items table

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,2 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`items` (`item_name`, `item_cost`) VALUES ("Call of Duty: Modern Warfare", 20.14);

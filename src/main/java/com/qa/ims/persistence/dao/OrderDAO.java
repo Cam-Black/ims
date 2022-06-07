3bae846e1fdc57f5859fd5ea0245d8966dbbf6f5
Cam-Black <cameronablack1998@gmail.com>
2022-06-07 20:33:37 +0100

    IMS-14 #comment functionality to read orders added, need further testing and manipulation of data to confirm functionality is working correctly

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -1,5 +1,83 @@
 package com.qa.ims.persistence.dao;
 
-public class OrderDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderDAO implements Dao<Order> {
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	
+	@Override
+	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long orderId = resultSet.getLong("Order ID");
+		
+//		Long itemId = resultSet.getLong("item_id");
+		String itemName = resultSet.getString("Product");
+		double itemCost = resultSet.getDouble("Item Price");
+		
+//		Long custId = resultSet.getLong("customer_id");
+		String custFName = resultSet.getString("First Name");
+		String custLName = resultSet.getString("Last Name");
+		
+		int itemQuantity = resultSet.getInt("Quantity");
+		
+		Customer customer = new Customer(custFName, custLName);
+		Item item = new Item(itemName, itemCost);
+		
+		return new Order(orderId, customer, item, itemQuantity);
+	}
+	
+	@Override
+	public List<Order> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT customers.first_name AS `First Name`, customers.surname AS `Last Name`, orders.order_id AS `Order ID`, items.item_name AS Product, SUM(order_items.item_quantity) AS Quantity, items.item_cost AS `Item Price` FROM order_items INNER JOIN orders ON orders.order_id = order_items.fk_order_id INNER JOIN items ON items.item_id = order_items.fk_item_id INNER JOIN customers ON customers.customer_id = orders.fk_customer_id;");) {
+			List<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(modelFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	@Override
+	public Order read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order update(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,16 +1,27 @@
 package com.qa.ims.persistence.domain;
 
 public class Order {
-	private Long order_id;
+	private Long orderId;
 	private Customer customer;
+	private Item item;
+	private int itemQuantity;
 	
-	public Order(Customer customer) {
-		super();
+	public Order(Customer customer, Item item, int itemQuantity) {
 		this.customer = customer;
+		this.item = item;
+		this.itemQuantity = itemQuantity;
 	}
 
-	public Order(Long order_id, Customer customer) {
-		this.order_id = order_id;
+	public Order(Long orderId, Customer customer, Item item, int itemQuantity) {
+		this.orderId = orderId;
 		this.customer = customer;
+		this.item = item;
+		this.itemQuantity = itemQuantity;
+	}
+	
+	@Override
+	public String toString() {
+		String name = customer.getFirstName() + " " + customer.getSurname();
+		return "orderId: " + orderId + ", Customer Name: " + name + ", Product: " + item.getItemName() + ", Quantity: " + itemQuantity + ", Total Cost: " + (item.getItemCost() * itemQuantity);
 	}
 }
